6d75be0620f7e3abad74646e59f37afbfa2ecaf8
Muhammad Samiullah <msami398@gmail.com>
2022-06-07 23:43:33 +0500

    Added sidebar to the hotels list and made search clickable

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -10,8 +10,10 @@ import "react-date-range/dist/styles.css";
 import "react-date-range/dist/theme/default.css";
 import { useState } from "react";
 import { format } from "date-fns";
+import { useNavigate } from "react-router-dom";
 
 function Search() {
+  const [destination, setDestination] = useState("");
   // for date range
   const [openDate, setOpenDate] = useState(false);
   const [date, setDate] = useState([
@@ -29,11 +31,21 @@ function Search() {
     room: 1,
   });
 
+  // use navigateHook from react-router-dom
+  const navigateHook = useNavigate();
+  const handleSearch = () => {
+    navigateHook("/hotels", { state: { destination, openDate, date } });
+  };
+
   const handleGuestOption = (name, operation) => {
-    setGuestOptions(prev=> { return {
-      ...prev, [name]: operation === "i" ? guestOptions[name] + 1: guestOptions[name] - 1
-    }})
-  }
+    setGuestOptions((prev) => {
+      return {
+        ...prev,
+        [name]:
+          operation === "i" ? guestOptions[name] + 1 : guestOptions[name] - 1,
+      };
+    });
+  };
 
   return (
     <div className="header-search">
@@ -43,6 +55,7 @@ function Search() {
           type="text"
           placeholder="where are you going?"
           className="header-search-input"
+          onChange={(e) => setDestination(e.target.value)}
         />
       </div>
       <div className="header-search-item">
@@ -66,36 +79,82 @@ function Search() {
       </div>
       <div className="header-search-item">
         <FontAwesomeIcon icon={faPerson} className="header-icon" />
-        <span onClick={() => setOpenGuest(!openGuest)} className="header-search-text">{`${guestOptions.adult} adult . ${guestOptions.children} children . ${guestOptions.room} room`}</span>
-        { openGuest && <div className="guest-options">
-          <div className="option-item">
-            <span className="option-text">Adult</span>
-            <div className="option-counter">
-              <button disabled={guestOptions.adult <= 1} className="option-counter-button" onClick={() => handleGuestOption("adult", "d")}>-</button>
-              <span className="option-counter-number">{guestOptions.adult}</span>
-              <button className="option-counter-button" onClick={() => handleGuestOption("adult", "i")}>+</button>
+        <span
+          onClick={() => setOpenGuest(!openGuest)}
+          className="header-search-text"
+        >{`${guestOptions.adult} adult . ${guestOptions.children} children . ${guestOptions.room} room`}</span>
+        {openGuest && (
+          <div className="guest-options">
+            <div className="option-item">
+              <span className="option-text">Adult</span>
+              <div className="option-counter">
+                <button
+                  disabled={guestOptions.adult <= 1}
+                  className="option-counter-button"
+                  onClick={() => handleGuestOption("adult", "d")}
+                >
+                  -
+                </button>
+                <span className="option-counter-number">
+                  {guestOptions.adult}
+                </span>
+                <button
+                  className="option-counter-button"
+                  onClick={() => handleGuestOption("adult", "i")}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <div className="option-item">
+              <span className="option-text">Children</span>
+              <div className="option-counter">
+                <button
+                  disabled={guestOptions.children <= 0}
+                  className="option-counter-button"
+                  onClick={() => handleGuestOption("children", "d")}
+                >
+                  -
+                </button>
+                <span className="option-counter-number">
+                  {guestOptions.children}
+                </span>
+                <button
+                  className="option-counter-button"
+                  onClick={() => handleGuestOption("children", "i")}
+                >
+                  +
+                </button>
+              </div>
+            </div>
+            <div className="option-item">
+              <span className="option-text">Room</span>
+              <div className="option-counter">
+                <button
+                  disabled={guestOptions.room <= 1}
+                  className="option-counter-button"
+                  onClick={() => handleGuestOption("room", "d")}
+                >
+                  -
+                </button>
+                <span className="option-counter-number">
+                  {guestOptions.room}
+                </span>
+                <button
+                  className="option-counter-button"
+                  onClick={() => handleGuestOption("room", "i")}
+                >
+                  +
+                </button>
+              </div>
             </div>
           </div>
-          <div className="option-item">
-            <span className="option-text">Children</span>
-            <div className="option-counter">
-              <button disabled={guestOptions.children <= 0} className="option-counter-button" onClick={() => handleGuestOption("children", "d")}>-</button>
-              <span className="option-counter-number">{guestOptions.children}</span>
-              <button className="option-counter-button" onClick={() => handleGuestOption("children", "i")}>+</button>
-            </div>
-          </div>
-          <div className="option-item">
-            <span className="option-text">Room</span>
-            <div className="option-counter">
-              <button disabled={guestOptions.room <= 1} className="option-counter-button" onClick={() => handleGuestOption("room", "d")}>-</button>
-              <span className="option-counter-number">{guestOptions.room}</span>
-              <button className="option-counter-button" onClick={() => handleGuestOption("room", "i")}>+</button>
-            </div>
-          </div>
-        </div> }
+        )}
       </div>
       <div className="header-search-item">
-        <button className="search-button header-btn">Search</button>
+        <button className="search-button header-btn" onClick={handleSearch}>
+          Search
+        </button>
       </div>
     </div>
   );
--- client/src/pages/list-hotels/ListHotels.jsx
+++ client/src/pages/list-hotels/ListHotels.jsx
@@ -1,12 +1,42 @@
 import "./list-hotels.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Header from "../../components/header/Header";
+import { useLocation } from "react-router-dom";
+import { useState } from "react";
+import { format } from "date-fns";
 
 function List() {
-  return <div>
-    <Navbar />
-    <Header type="list-hotels"/>
-  </div>;
+  const location = useLocation();
+  console.log(location);
+  const [destination, setDestination] = useState(location.state.destination);
+  const [openDate, setOpenDate] = useState(location.state.openDate);
+  const [date, setDate] = useState(location.state.date);
+
+  return (
+    <div>
+      <Navbar />
+      <Header type="list-hotels" />
+      <div className="list-container">
+        <div className="list-wrapper">
+          <div className="list-search">
+            <h1 className="list-search-title">Search</h1>
+            <div className="list-search-item">
+              <label>Destination</label>
+              <input type="text" />
+            </div>
+            <div className="list-search-item">
+              <label>Check-in Date</label>
+              <span>{`${format(date[0].startDate, "MM/dd/yyyy")} to ${format(
+                date[0].endDate,
+                "MM/dd/yyyy"
+              )}`}</span>
+            </div>
+          </div>
+          <div className="list-result"></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default List;
